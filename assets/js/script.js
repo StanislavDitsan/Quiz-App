fc8054629bb0a0c0185a148ac1c683b293a55792
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-08-10 18:35:15 +0000

    Added list of qustions with option and answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,61 @@ const option = Array.from(document.getElementsByClassName("option-text"));
 
 // Variables.
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+// Question field.
+let questions = [
+    {
+        question: "What is the word Halloween an abbreviation of?",
+        option1: "All Hallow's Eve",
+        option2: "All Hallows Day",
+        option3: "All Saints Day",
+        option4: "On All Saints' Eve",
+        answer: 1
+    },
+    {
+        question: "What is a group of witches called?",
+        option1: "Pack of witches",
+        option2: "A coven",
+        option3: "Enchantress",
+        option4: "A convent",
+        answer: 2
+    },
+    {
+        question: "What would you traditionally bob for at Halloween parties?",
+        option1: "Pumpkin",
+        option2: "Grapes",
+        option3: "Oranges",
+        option4: "Apples",
+        answer: 4
+    },
+    {
+        question: "Who wrote the novel “Dracula”?",
+        option1: "Bram Stoker",
+        option2: "Ben Lerner",
+        option3: "Walt Whitman",
+        option4: "William Butler Yeats",
+        answer: 1
+    },
+    {
+        question: "Which actor made his film debut in A Nightmare on Elm Street?",
+        option1: "Johnny Depp",
+        option2: "Michael Myers",
+        option3: "Tom Hanks",
+        option4: "Jim Carrey",
+        answer: 1
+    },
+    {
+        question: "Who is the villain in Halloween?",
+        option1: "Tom Atkins",
+        option2: "Michael Myers",
+        option3: "John Carpenter",
+        option4: "Ed Gein",
+        answer: 2
+    },
+    
+]
 
