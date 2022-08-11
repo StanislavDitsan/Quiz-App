efb3627f5b7b0c18ec207e6b8c3384344019ea92
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-08-11 22:15:34 +0000

    Created endGame.html and endGame.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,7 +107,7 @@ getNewQuestion = () => {
 
     // When out of questions moves to the end page.
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
-        return window.location.assign('/end.html');
+        return window.location.assign('endGame.html');
     }
 };
 
