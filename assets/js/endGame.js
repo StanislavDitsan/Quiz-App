dabcea88d63dc51ac0b61f8b2bcc5f8f69eed981
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-08-12 19:13:39 +0000

    Added save high scores to the local storage

--- assets/js/endGame.js
+++ assets/js/endGame.js
@@ -3,15 +3,30 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('finalScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const maxHighScores = 5;
 
 finalScore.innerText = mostRecentScore;
-username.addEventListener("keyup", ()=> {
-    //Disables the submission if no name is provided.
+username.addEventListener("keyup", () => {
+    //Disables and enables the submission if no name is provided.
     saveScoreBtn.disabled = !username.value;
 });
 
 //Defines save score function
 saveHighScore = i => {
-i.preventDefault();
+    i.preventDefault();
 
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a, b) => {
+        return b.score - a.score;
+    });
+    highScores.splice(5);
+
+//Saves as a string for the highScores
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html");
 };
